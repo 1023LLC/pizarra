82aaa7af1a4a93b080e9a7e731ae4f023beeb2e9
julius <nyerereoffice@gmail.com>
2025-01-22 20:37:54 +0300

    Activate Links

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.index, name='indes'),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 ]
